8c790b45c91dac09f10f793b08bbe690d3e28016
子烟 杜 <907500395@qq.com>
2019-09-28 22:47:48 +0800

    添加项目文件。

---
+++ Common/T4Helper/DBHelper/MysqlDbhelper.cs
@@ -0,0 +1,182 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.T4Helper.DBHelper
+{
+    public class EntityHelper
+    {
+        public static List<Entity> GetEntities(string connectionString, List<string> databases)
+        {
+            var list = new List<Entity>();
+
+            // 数据库连接
+            var conn = new MySqlConnection(@"server=127.0.0.1;uid=root;pwd=123456;charset=utf8;");
+
+            // 需要解析的数据库
+            var database = new List<string> { "fyt_cms" };
+            try
+            {
+                conn.Open();
+                var dbs = string.Join("','", databases.ToArray());
+                var cmd = string.Format(@"SELECT `information_schema`.`COLUMNS`.`TABLE_SCHEMA`
+                                                    ,`information_schema`.`COLUMNS`.`TABLE_NAME`
+                                                    ,`information_schema`.`COLUMNS`.`COLUMN_NAME`
+                                                    ,`information_schema`.`COLUMNS`.`DATA_TYPE`
+                                                    ,`information_schema`.`COLUMNS`.`COLUMN_COMMENT`
+                                                    ,`information_schema`.`COLUMNS`.`COLUMN_KEY`
+                                                FROM `information_schema`.`COLUMNS`
+                                                WHERE `information_schema`.`COLUMNS`.`TABLE_SCHEMA` IN ('{0}') ", dbs);
+                using (var reader = MySqlHelper.ExecuteReader(conn, cmd))
+                {
+                    while (reader.Read())
+                    {
+                        var db = reader["TABLE_SCHEMA"].ToString();
+                        var table = reader["TABLE_NAME"].ToString();
+                        var column = reader["COLUMN_NAME"].ToString();
+                        var type = reader["DATA_TYPE"].ToString();
+                        var comment = reader["COLUMN_COMMENT"].ToString();
+                        var columnKey = reader["COLUMN_KEY"].ToString();
+                        var entity = list.FirstOrDefault(x => x.TableName == table);
+                        if (entity == null)
+                        {
+                            entity = new Entity(table);
+                            entity.Fields.Add(new Field
+                            {
+                                Name = column,
+                                Type = GetCLRType(type),
+                                Comment = comment,
+                                ColumnKey = columnKey
+                            });
+
+                            list.Add(entity);
+                        }
+                        else
+                        {
+                            entity.Fields.Add(new Field
+                            {
+                                Name = column,
+                                Type = GetCLRType(type),
+                                Comment = comment,
+                                ColumnKey = columnKey
+                            });
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return list;
+        }
+
+
+
+        public static string GetCLRType(string dbType)
+        {
+            switch (dbType)
+            {
+                case "bigint":
+                    return "long";
+                case "tinyint":
+                case "smallint":
+                case "mediumint":
+                case "int":
+                case "integer":
+                    return "int";
+                case "double":
+                    return "double";
+                case "float":
+                    return "float";
+                case "decimal":
+                    return "decimal";
+                case "numeric":
+                case "real":
+                    return "decimal";
+                case "bit":
+                    return "bool";
+                case "date":
+                case "time":
+                case "year":
+                case "datetime":
+                case "timestamp":
+                    return "DateTime";
+                case "tinyblob":
+                case "blob":
+                case "mediumblob":
+                case "longblog":
+                case "binary":
+                case "varbinary":
+                    return "byte[]";
+                case "char":
+                case "varchar":
+                case "tinytext":
+                case "text":
+                case "mediumtext":
+                case "longtext":
+                    return "string";
+                case "point":
+                case "linestring":
+                case "polygon":
+                case "geometry":
+                case "multipoint":
+                case "multilinestring":
+                case "multipolygon":
+                case "geometrycollection":
+                case "enum":
+                case "set":
+                default:
+                    return dbType;
+            }
+        }
+    }
+
+    public class Entity
+    {
+        public Entity()
+        {
+            this.Fields = new List<Field>();
+        }
+
+        public Entity(string name)
+            : this()
+        {
+            this.TableName = name;
+            this.EntityName = TableNameConvertModelName(name);
+        }
+
+        public string EntityName { get; set; }
+        public string TableName { get; set; }
+        public List<Field> Fields { get; set; }
+
+        public static string TableNameConvertModelName(string name)
+        {
+            var modelName = string.Empty;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return name;
+            }
+            var arrName = name.Split('_');
+            if (arrName.Any())
+            {
+                foreach (var m in arrName)
+                {
+                    modelName += string.IsNullOrWhiteSpace(m) ? "" : m.Substring(0, 1).ToUpper() + m.Substring(1);
+                }
+            }
+            return modelName;
+        }
+    }
+
+    public class Field
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        public string Comment { get; set; }
+
+        public string ColumnKey { get; set; }
+    }
+}
---
+++ T4MysqlDemo/EntityHelper1.cs
@@ -0,0 +1,177 @@
+﻿using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T4MysqlDemo
+{
+    public class EntityHelper
+    {
+        public static List<Entity> GetEntities(string connectionString, List<string> databases)
+        {
+            var list = new List<Entity>();
+            var conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                var dbs = string.Join("','", databases.ToArray());
+                var cmd = string.Format(@"SELECT `information_schema`.`COLUMNS`.`TABLE_SCHEMA`
+                                                    ,`information_schema`.`COLUMNS`.`TABLE_NAME`
+                                                    ,`information_schema`.`COLUMNS`.`COLUMN_NAME`
+                                                    ,`information_schema`.`COLUMNS`.`DATA_TYPE`
+                                                    ,`information_schema`.`COLUMNS`.`COLUMN_COMMENT`
+                                                    ,`information_schema`.`COLUMNS`.`COLUMN_KEY`
+                                                FROM `information_schema`.`COLUMNS`
+                                                WHERE `information_schema`.`COLUMNS`.`TABLE_SCHEMA` IN ('{0}') ", dbs);
+                using (var reader = MySqlHelper.ExecuteReader(conn, cmd))
+                {
+                    while (reader.Read())
+                    {
+                        var db = reader["TABLE_SCHEMA"].ToString();
+                        var table = reader["TABLE_NAME"].ToString();
+                        var column = reader["COLUMN_NAME"].ToString();
+                        var type = reader["DATA_TYPE"].ToString();
+                        var comment = reader["COLUMN_COMMENT"].ToString();
+                        var columnKey = reader["COLUMN_KEY"].ToString();
+                        var entity = list.FirstOrDefault(x => x.TableName == table);
+                        if (entity == null)
+                        {
+                            entity = new Entity(table);
+                            entity.Fields.Add(new Field
+                            {
+                                Name = column,
+                                Type = GetCLRType(type),
+                                Comment = comment,
+                                ColumnKey = columnKey
+                            });
+
+                            list.Add(entity);
+                        }
+                        else
+                        {
+                            entity.Fields.Add(new Field
+                            {
+                                Name = column,
+                                Type = GetCLRType(type),
+                                Comment = comment,
+                                ColumnKey = columnKey
+                            });
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return list;
+        }
+
+
+
+        public static string GetCLRType(string dbType)
+        {
+            switch (dbType)
+            {
+                case "bigint":
+                    return "long";
+                case "tinyint":
+                case "smallint":
+                case "mediumint":
+                case "int":
+                case "integer":
+                    return "int";
+                case "double":
+                    return "double";
+                case "float":
+                    return "float";
+                case "decimal":
+                    return "decimal";
+                case "numeric":
+                case "real":
+                    return "decimal";
+                case "bit":
+                    return "bool";
+                case "date":
+                case "time":
+                case "year":
+                case "datetime":
+                case "timestamp":
+                    return "DateTime";
+                case "tinyblob":
+                case "blob":
+                case "mediumblob":
+                case "longblog":
+                case "binary":
+                case "varbinary":
+                    return "byte[]";
+                case "char":
+                case "varchar":
+                case "tinytext":
+                case "text":
+                case "mediumtext":
+                case "longtext":
+                    return "string";
+                case "point":
+                case "linestring":
+                case "polygon":
+                case "geometry":
+                case "multipoint":
+                case "multilinestring":
+                case "multipolygon":
+                case "geometrycollection":
+                case "enum":
+                case "set":
+                default:
+                    return dbType;
+            }
+        }
+    }
+
+    public class Entity
+    {
+        public Entity()
+        {
+            this.Fields = new List<Field>();
+        }
+
+        public Entity(string name)
+            : this()
+        {
+            this.TableName = name;
+            this.EntityName = TableNameConvertModelName(name);
+        }
+
+        public string EntityName { get; set; }
+        public string TableName { get; set; }
+        public List<Field> Fields { get; set; }
+
+        public static string TableNameConvertModelName(string name)
+        {
+            var modelName = string.Empty;
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return name;
+            }
+            var arrName = name.Split('_');
+            if (arrName.Any())
+            {
+                foreach (var m in arrName)
+                {
+                    modelName += string.IsNullOrWhiteSpace(m) ? "" : m.Substring(0, 1).ToUpper() + m.Substring(1);
+                }
+            }
+            return modelName;
+        }
+    }
+
+    public class Field
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+        public string Comment { get; set; }
+
+        public string ColumnKey { get; set; }
+    }
+}
